106377e81899aa7f34e134e4ddae05109fd05243
guilorentz <go.lorentz@gmail>
2024-10-07 21:39:32 -0400

    chore: add angular material

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,12 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideEnvironmentNgxMask(),
+    provideEnvironmentNgxMask(), provideAnimationsAsync(),
   ],
 }).catch((err) => console.error(err));
