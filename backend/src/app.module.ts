ef7470dc3c10beae3cfa47f22eba87bb9b4bd3dc
guilorentz <go.lorentz@gmail>
2024-10-08 09:07:13 -0400

    chore: remove unused files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
-import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CompaniesModule, AuthModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CompaniesModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
